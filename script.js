49ab437d114576b1e53ea896895b9bbf15b1a60b
JoanChirinos <jchirinos@stuy.edu>
2018-10-01 18:31:43 -0400

    added error msg

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+var error = document.getElementById('error');
+
 var create_account_request = new XMLHttpRequest();
 
 create_account_request.onload = function () {
     var return_val = create_account_request.responseText;
     console.log(return_val);
+    if (return_val == '0') {
+        error.innerHTML = 'That username already exists!';
+        return;
+    }
+    // if new account created
+    else if (return_val == '1') {
+        error.innerHTML = 'Account created! Please log in!'
+        document.getElementById('password').value = '';
+    }
 }
 
 function create_account() {
 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    var error = document.getElementById('error')
 
     var re = /^[a-z0-9]+$/i;
     if (!(re.test(username)) || !(re.test(password))) {
