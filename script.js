e0df1149dacbba90492206110c526e3f7f12d2c8
JoanChirinos <jchirinos@stuy.edu>
2018-10-01 18:35:31 -0400

    .

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ create_account_request.onload = function () {
     var return_val = create_account_request.responseText;
     console.log(return_val);
     if (return_val == '0') {
+        console.log('username exists');
         error.innerHTML = 'That username already exists!';
         return;
     }
     // if new account created
     else if (return_val == '1') {
+        console.log('account created');
         error.innerHTML = 'Account created! Please log in!';
         document.getElementById('password').value = '';
+        return;
     }
 }
 
@@ -27,6 +30,6 @@ function create_account() {
         return;
     }
 
-    create_account_request.open("GET", 'http://homer.stuy.edu/~jchirinos/MediaPlatform/data/create_account.py?username=' + username + '&password=' + password, true);
+    create_account_request.open("GET", 'http://homer.stuy.edu/~jchirinos/MediaPlatform/data/create_account.py?username=' + username + '&password=' + password);
     create_account_request.send();
 }
